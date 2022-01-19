fc3ccd65bdafc3a2c0387d5982801f9162cc7fd5
Lucie Griveau <lucie.griveau@le-campus-numerique.fr>
2022-01-19 18:06:13 +0100

    modification des routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ArgumentController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MyTVController;
+use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\StationController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideoController;
@@ -21,35 +23,44 @@ use App\Http\Controllers\VideoController;
 |
 */
 
-//Route to get homepage view
+// Routes to get homepage view
 Route::get('/', function () {
     return view('homepage');
 });
+Route::get('/{userID}', [UserController::class, 'userLogged']);
 
 // Routes to station views
 Route::get('/channels', [StationController::class, 'stations']);
-Route::get('/channels/{user_id}', [StationController::class, 'stations']);
-Route::get('/channel/{station_id}', [StationController::class, 'station']);
+Route::get('/channels/{userID}', [StationController::class, 'userStations']);
+Route::get('/channel/{stationID}', [StationController::class, 'station']);
 //get('/channel/{station_slug}'
 
-// Route to contents view
+// Routes to contents view
 Route::get('/contents', [VideoController::class, 'contents']);
-Route::get('/contents/{user_id}', [VideoController::class, 'contents']);
+Route::get('/contents/{userID}', [VideoController::class, 'userContents']);
 
-// Routes to myAEngieTv views
-Route::get('/mytv/{user_id}', [MyTVController::class, 'userTV']);
-Route::get('/mytv/{user_id}/showreel', [MyTVController::class, 'userShowreel']);
-Route::get('/mytv/{user_id}/library', [MyTVController::class, 'userLibrary']);
-Route::get('/mytv/{user_id}/notifications', [MyTVController::class, 'userNotifications']);
-Route::get('/mytv/{user_id}/user_settings', [MyTVController::class, 'userSettings']);
+// Routes to myAEngieTV views
+Route::get('/mytv/{userID}', [MyTVController::class, 'userTV']);
+Route::get('/mytv/{userID}/showreel', [MyTVController::class, 'userShowreel']);
+Route::get('/mytv/{userID}/library', [MyTVController::class, 'userLibrary']);
+Route::get('/mytv/{userID}/notifications', [MyTVController::class, 'userNotifications']);
+Route::get('/mytv/{userID}/user_settings', [MyTVController::class, 'userSettings']);
 
-Route::get('/mytv/{user_id}/dashboard/', [MyTVController::class, 'selectStation']);
-Route::get('/mytv/{user_id}/dashboard/{station_id}', [MyTVController::class, 'userDashboard']);
-Route::get('/mytv/{user_id}/dashboard/contributions', [MyTVController::class, 'contributions']);
-Route::get('/mytv/{user_id}/dashboard/settings/community', [MyTVController::class, 'communitySettings']);
+// Routes to myAEngieTV > Dashboard views
+Route::get('/mytv/{userID}/dashboard/', [DashboardController::class, 'selectStation']);
+//Route::get('/mytv/{userID}/dashboard/{stationID}', [DashboardController::class, 'dashboard']);
+Route::get('/mytv/{userID}/dashboard/community', [DashboardController::class, 'community']);
+Route::get('/mytv/{userID}/dashboard/contributions', [DashboardController::class, 'contributions']);
+Route::get('/mytv/{userID}/dashboard/playlist', [DashboardController::class, 'playlist']);
 
-// Route to contacts view
-Route::get('/support', [CategoryController::class, 'categoryDisplay']);
+// Routes to myAEngieTV > Dashboard > Settings views
+Route::get('/mytv/{userID}/dashboard/settings/event', [SettingsController::class, 'broadcastSettings']);
+Route::get('/mytv/{userID}/dashboard/settings/community', [SettingsController::class, 'communitySettings']);
+Route::get('/mytv/{userID}/dashboard/settings/station', [SettingsController::class, 'stationSettings']);
+
+// Routes to contacts view
+Route::get('/support', [CategoryController::class, 'categories']);
+Route::get('/support/{userID}', [CategoryController::class, 'userCategories']);
 
 // Route to register
 Route::get('/register', function () {
